6c44a004a3a73575728aa2f6e3301851a018144c
Charles Buxton <chazbux@gmail.com>
2020-02-15 15:26:38 -0700

    changed structure of casino app to pull from account.rb.

---
+++ account.rb
@@ -0,0 +1,54 @@
+class Account
+  attr_accessor :name
+  def initialize
+    @bankroll = []
+    @name = name
+  end
+  
+  def get_name
+    puts "Please enter your username: "
+    @name = gets.strip
+  end
+
+  def get_cash
+    
+    cash = gets.to_f
+    @bankroll.push(cash)
+    
+    puts "Your starting balance is $#{sprintf("%.2f",@bankroll[0])}."
+  end
+
+  def add_money
+    # Prompts user to increase their account balance.
+    puts "Enter amount"
+    amount = gets.to_f
+    @bankroll.push(amount)
+  end
+
+  def sum_bankroll
+    # This prints a sum of all items in the array.
+    @sum = @bankroll.inject(0) {|result, element|result + element}
+    puts @sum
+  end
+    
+  def place_bet
+    # Prompts user for a bet amount, compares bet amount to current account balance (@bankroll).
+    @bet = gets.to_f
+    if @bet <= @sum
+      # go
+    else 
+      puts "Insuffient Funds"
+      # go to menu      
+    end
+
+  end
+
+
+end
+
+
+
+
+
+
+
--- app.rb
+++ app.rb
@@ -1,62 +1,23 @@
 require_relative 'deck'
 require_relative 'dice'
 require_relative 'card'
-
-class Player
-  attr_accessor :name # Do we need this?
-    def inititalize
-      @name = name
-    end
-    def get_name
-      @name = gets.strip
-    end
-  end
+require_relative 'account'
 
 
-class Wallet
-  attr_accessor :wallet # Do we need this?
-  def inititalize
-    @wallet = wallet
-    @bet = bet
-    @bankroll = []
-  end
-  
-  def get_wallet
-    @wallet = gets.to_f
-    @bankroll << @wallet
-  end
-
-  def bet
-    @bet = gets.to_f
-    if @bet # < @Bankroll
-      # go
-    else @bet # > @Bankroll
-      puts "Insuffient Funds"
-      # go to menu      
-    end
-
-    def get_money
-      puts "Enter amount"
-      amount = gets.to_f
-      @bankroll.push(amount)
-    end
-
-  end
-end
   
 class Casino
   def app
+    puts "**********************************"
     puts "Welcome to the Honeybadger Casino!"
-    puts "Enter your name"
-    new_player = Player.new
+    puts "**********************************"
+    new_player = Account.new
     new_player.get_name
     puts "Hello #{new_player.name}, enter how much money your ready to lose."
-    new_wallet = Wallet.new
-    new_wallet.get_wallet
-    puts "Your starting balance is #{sprintf("%.2f",new_wallet.wallet)}"
+    new_player.get_cash
+  
     # menu
   end
 end
 
 game = Casino.new
-game.app
+game.app()
